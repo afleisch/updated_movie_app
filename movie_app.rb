363a97a915afe428ceb2dde641a2c398d0aede02
afleisch <afleisch@gmail.com>
2014-01-17 17:19:58 -0800

    phase two complete!!

--- movie_app.rb
+++ movie_app.rb
@@ -2,35 +2,32 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
+require 'pry'
 
-get "/" do 
+get '/' do #at the home page, render code in index.erb 
+#i.e form to enter search paramters
     erb :index	
 end
 
-post "/result" do #send the information enter into search and return it to /result page
+post '/result' do #send the information entered into search 
+#and return it to /result page via result.erb
 
-search_str = params[:movie]
-response = Typhoeus.get("www.omdbapi.com", :params => {:s => search_str})
-
-@initial_movie_array = JSON.parse(response.body)["Search"]
-erb :result
-
+	search_str = params[:movie]
+	response = Typhoeus.get("www.omdbapi.com", :params => {:s => search_str})
+	binding.pry
+	@initial_data = JSON.parse(response.body)
+	
+	@movie_array_of_hashes = @initial_data["Search"]
+	erb :result
 end
 
-post '/test' do
-
-  search_str = params[:movie]
-  response = Typhoeus.get("www.omdbapi.com", :params => {s: search_str})
-  movies = JSON.parse(response.body)["Search"]
-
-end
-
-get '/movieinfo/:imdb' do |imdb_id|
-  # Make another api call here to get the url of the poster.
+get '/poster/:imdb' do |imdb_id| #get list of information from api 
+#request based on search parameter equal to imdb_id and display it to
+#/movieinfo page via movieinfo.erb
+ 
   response = Typhoeus.get("www.omdbapi.com", :params => {i: imdb_id})
   @movieinfo = JSON.parse(response.body)
- 
 
-  erb :movieinfo
+  erb :poster
 
 end
