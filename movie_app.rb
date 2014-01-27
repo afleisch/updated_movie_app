61a7289070f96fabf1a964a0a72f03cd4d7760a0
afleisch <afleisch@gmail.com>
2014-01-26 20:19:56 -0800

    adjusted indentation and spacing

--- movie_app.rb
+++ movie_app.rb
@@ -1,4 +1,4 @@
-require 'sinatra'
+require 'sinatra' 
 require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
@@ -12,29 +12,31 @@ end
 post '/result' do #send the information entered into search 
 #and return it to /result page via result.erb
 
-	search_str = params[:movie]
-	response = Typhoeus.get("www.omdbapi.com", :params => {:s => search_str})
-	binding.pry
-	@initial_data = JSON.parse(response.body)
-	if @initial_data['Response'] == "False"
-		redirect "/"
-	else
-	@movie_array_of_hashes = @initial_data["Search"]
-	@just_movies =[]
-	@movie_array_of_hashes.select do |movie_hash|
-		if movie_hash["Type"] == "movie"
-			@just_movies << movie_hash
-		end
-	  end
+  search_str = params[:movie]
+  response = Typhoeus.get("www.omdbapi.com", :params => {:s => search_str})
+  binding.pry
+  @initial_data = JSON.parse(response.body)
+  @bad_results = "a string"
+  #if @initial_data['Response'] == "False"
+    @bad_results = true
+	  redirect "/"
+	#else
+  @movie_array_of_hashes = @initial_data["Search"]
+  @just_movies =[]
+  @movie_array_of_hashes.select do |movie_hash|
+    if movie_hash["Type"] == "movie"
+	  @just_movies << movie_hash
 	end
+	end
+	#end
 
-	erb :result
-end
+  erb :result
+  end
 
 get '/poster/:imdb' do |imdb_id| #get list of information from api 
 #request based on search parameter equal to imdb_id and display it to
 #/movieinfo page via movieinfo.erb
- 
+
   response = Typhoeus.get("www.omdbapi.com", :params => {i: imdb_id})
   @movieinfo = JSON.parse(response.body)
 
